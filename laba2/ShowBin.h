1a9087b20d6af96fcc6583b857f9ae04e09a37ba
Cpt0bvious <51872711+Cpt0bvious@users.noreply.github.com>
2019-06-18 20:26:59 +0700

    Create ShowBin.h

---
+++ laba2/ShowBin.h
@@ -0,0 +1,11 @@
+#include "Action.h"
+
+class ShowBin : public Action {
+public:
+	ShowBin() : name("Show binary value") {}
+	void Operate(AString*);
+	const std::string& GetName() const{ return name; }
+private:
+	std::string GetBinary(AString*) const;
+	std::string name; 
+};
